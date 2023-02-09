98658230b21459dd74b72156738c6f5249e2f6ac
MIguel-Bill <sodumbdontdrink11@gmail.com>
2023-02-09 01:44:28 +0000

    this commit is my first script

---
+++ Helloworld.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Miguel Angel Lopez Villegas
+# Date of latest revision: 02/08/2023
+# Purpose: Print a string to the terminal
+
+
+# Main
+echo "I am working perfectly"
+# end
